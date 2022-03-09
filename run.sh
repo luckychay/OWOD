3b06a0d4a75f93daf6627ebf55c65a3e87254bfe
luckychay <872232408@qq.com>
2022-03-09 09:39:49 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 # General flow: tx_train.yaml -> tx_ft -> tx_val -> tx_test
+###
+ # @Description: 
+ # @Version: 
+ # @Author: Xuanying Chen
+ # @Date: 2022-03-09 09:34:42
+ # @LastEditTime: 2022-03-09 09:39:04
+### 
 
 # tx_train: trains the model.
 # tx_ft: uses data-replay to address forgetting. (refer Sec 4.4 in paper)
@@ -11,50 +18,50 @@
 # (if you find something wrong in this, please raise an issue on GitHub)
 
 # Task 1
-python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52125' --resume --config-file ./configs/OWOD/t1/t1_train.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t1"
+python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52125' --resume --config-file ./configs/OWOD/t1/t1_train.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.00125 OUTPUT_DIR "../drive/MyDrive/output/t1"
 
 # No need to finetune in Task 1, as there is no incremental component.
 
-python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t1/t1_val.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OWOD.TEMPERATURE 1.5 OUTPUT_DIR "./output/t1_final" MODEL.WEIGHTS "/home/joseph/workspace/OWOD/output/t1/model_final.pth"
+python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t1/t1_val.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.00125 OWOD.TEMPERATURE 1.5 OUTPUT_DIR "../drive/MyDrive/output/t1_final" MODEL.WEIGHTS "../drive/MyDrive/output/t1/model_final.pth"
 
-python tools/train_net.py --num-gpus 8 --eval-only --config-file ./configs/OWOD/t1/t1_test.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.005 OUTPUT_DIR "./output/t1_final" MODEL.WEIGHTS "/home/joseph/workspace/OWOD/output/t1/model_final.pth"
+python tools/train_net.py --num-gpus 1 --eval-only --config-file ./configs/OWOD/t1/t1_test.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.005 OUTPUT_DIR "../drive/MyDrive/output/t1_final" MODEL.WEIGHTS "../drive/MyDrive/output/t1/model_final.pth"
 
 
 # Task 2
-cp -r /home/joseph/workspace/OWOD/output/t1 /home/joseph/workspace/OWOD/output/t2
+cp -r ../drive/MyDrive/output/t1 ../drive/MyDrive/output/t2
 
-python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52127' --resume --config-file ./configs/OWOD/t2/t2_train.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t2" MODEL.WEIGHTS "/home/joseph/workspace/OWOD/output/t2/model_final.pth"
+python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52127' --resume --config-file ./configs/OWOD/t2/t2_train.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.00125 OUTPUT_DIR "../drive/MyDrive/output/t2" MODEL.WEIGHTS "../drive/MyDrive/output/t2/model_final.pth"
 
-cp -r /home/joseph/workspace/OWOD/output/t2 /home/joseph/workspace/OWOD/output/t2_ft
+cp -r ../drive/MyDrive/output/t2 ../drive/MyDrive/output/t2_ft
 
-python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52126' --resume --config-file ./configs/OWOD/t2/t2_ft.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t2_ft" MODEL.WEIGHTS "/home/joseph/workspace/OWOD/output/t2_ft/model_final.pth"
+python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52126' --resume --config-file ./configs/OWOD/t2/t2_ft.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.00125 OUTPUT_DIR "../drive/MyDrive/output/t2_ft" MODEL.WEIGHTS "../drive/MyDrive/output/t2_ft/model_final.pth"
 
-python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t2/t2_val.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OWOD.TEMPERATURE 1.5 OUTPUT_DIR "./output/t2_final" MODEL.WEIGHTS "/home/joseph/workspace/OWOD/output/t2_ft/model_final.pth"
+python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t2/t2_val.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.00125 OWOD.TEMPERATURE 1.5 OUTPUT_DIR "../drive/MyDrive/output/t2_final" MODEL.WEIGHTS "../drive/MyDrive/output/t2_ft/model_final.pth"
 
-python tools/train_net.py --num-gpus 8 --eval-only --config-file ./configs/OWOD/t2/t2_test.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.005 OUTPUT_DIR "./output/t2_final" MODEL.WEIGHTS "/home/joseph/workspace/OWOD/output/t2_ft/model_final.pth"
+python tools/train_net.py --num-gpus 1 --eval-only --config-file ./configs/OWOD/t2/t2_test.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.005 OUTPUT_DIR "../drive/MyDrive/output/t2_final" MODEL.WEIGHTS "../drive/MyDrive/output/t2_ft/model_final.pth"
 
 
 # Task 3
-cp -r /home/joseph/workspace/OWOD/output/t2_ft /home/joseph/workspace/OWOD/output/t3
+cp -r ../drive/MyDrive/output/t2_ft ../drive/MyDrive/output/t3
 
-python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52127' --resume --config-file ./configs/OWOD/t3/t3_train.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t3" MODEL.WEIGHTS "/home/joseph/workspace/OWOD/output/t3/model_final.pth"
+python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52127' --resume --config-file ./configs/OWOD/t3/t3_train.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.00125 OUTPUT_DIR "../drive/MyDrive/output/t3" MODEL.WEIGHTS "../drive/MyDrive/output/t3/model_final.pth"
 
-cp -r /home/joseph/workspace/OWOD/output/t3 /home/joseph/workspace/OWOD/output/t3_ft
+cp -r ../drive/MyDrive/output/t3 ../drive/MyDrive/output/t3_ft
 
-python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52126' --resume --config-file ./configs/OWOD/t3/t3_ft.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t3_ft" MODEL.WEIGHTS "/home/joseph/workspace/OWOD/output/t3_ft/model_final.pth"
+python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52126' --resume --config-file ./configs/OWOD/t3/t3_ft.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.00125 OUTPUT_DIR "../drive/MyDrive/output/t3_ft" MODEL.WEIGHTS "../drive/MyDrive/output/t3_ft/model_final.pth"
 
-python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t3/t3_val.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OWOD.TEMPERATURE 1.5 OUTPUT_DIR "./output/t3_final" MODEL.WEIGHTS "/home/joseph/workspace/OWOD/output/t3_ft/model_final.pth"
+python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t3/t3_val.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.00125 OWOD.TEMPERATURE 1.5 OUTPUT_DIR "../drive/MyDrive/output/t3_final" MODEL.WEIGHTS "../drive/MyDrive/output/t3_ft/model_final.pth"
 
-python tools/train_net.py --num-gpus 8 --eval-only --config-file ./configs/OWOD/t3/t3_test.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.005 OUTPUT_DIR "./output/t3_final" MODEL.WEIGHTS "/home/joseph/workspace/OWOD/output/t3_ft/model_final.pth"
+python tools/train_net.py --num-gpus 1 --eval-only --config-file ./configs/OWOD/t3/t3_test.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.005 OUTPUT_DIR "../drive/MyDrive/output/t3_final" MODEL.WEIGHTS "../drive/MyDrive/output/t3_ft/model_final.pth"
 
 
 # Task 4
-cp -r /home/joseph/workspace/OWOD/output/t3_ft /home/joseph/workspace/OWOD/output/t4
+cp -r ../drive/MyDrive/output/t3_ft ../drive/MyDrive/output/t4
 
-python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52127' --resume --config-file ./configs/OWOD/t4/t4_train.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t4" MODEL.WEIGHTS "/home/joseph/workspace/OWOD/output/t4/model_final.pth"
+python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52127' --resume --config-file ./configs/OWOD/t4/t4_train.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.00125 OUTPUT_DIR "../drive/MyDrive/output/t4" MODEL.WEIGHTS "../drive/MyDrive/output/t4/model_final.pth"
 
-cp -r /home/joseph/workspace/OWOD/output/t4 /home/joseph/workspace/OWOD/output/t4_ft
+cp -r ../drive/MyDrive/output/t4 ../drive/MyDrive/output/t4_ft
 
-python tools/train_net.py --num-gpus 8 --dist-url='tcp://127.0.0.1:52126' --resume --config-file ./configs/OWOD/t4/t4_ft.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.01 OUTPUT_DIR "./output/t4_ft" MODEL.WEIGHTS "/home/joseph/workspace/OWOD/output/t4_ft/model_final.pth"
+python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52126' --resume --config-file ./configs/OWOD/t4/t4_ft.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.00125 OUTPUT_DIR "../drive/MyDrive/output/t4_ft" MODEL.WEIGHTS "../drive/MyDrive/output/t4_ft/model_final.pth"
 
-python tools/train_net.py --num-gpus 8 --eval-only --config-file ./configs/OWOD/t4/t4_test.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.005 OUTPUT_DIR "./output/t4_final" MODEL.WEIGHTS "/home/joseph/workspace/OWOD/output/t4_ft/model_final.pth"
+python tools/train_net.py --num-gpus 1 --eval-only --config-file ./configs/OWOD/t4/t4_test.yaml SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.005 OUTPUT_DIR "../drive/MyDrive/output/t4_final" MODEL.WEIGHTS "../drive/MyDrive/output/t4_ft/model_final.pth"
